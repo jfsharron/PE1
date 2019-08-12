9f09b476a80ac023907dabcb01490a19e255604f
Jonas Sharron 205 <jfsharron@msn.com>
2019-08-12 10:39:10 -0700

    added code for problem

--- ProjectEuler1/source.c
+++ ProjectEuler1/source.c
@@ -20,3 +20,22 @@
 #include<string.h> 
 #include<stdlib.h> 
 
+int main() {
+
+	int max = 1000;
+	int sum = 0;
+
+	for (int i = 0; i < max; i++) {
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = (i + sum);
+		}
+	}
+
+	printf("\nThe sum is: %d\n", sum);
+
+
+
+
+
+}  //  end main
